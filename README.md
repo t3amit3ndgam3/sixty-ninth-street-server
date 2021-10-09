cd00b8954a25ebf127f1ac86cd7cbbe474ad0732
Unmoy <vampireklug@gmail.com>
2021-10-09 19:50:41 +0600

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sixty-ninth-street-server
+
+Owner 1. Golam rabby
+Owner 2. Unmoy Biswass
--- index.js
+++ index.js
@@ -1,35 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const port = 5000;
-const { MongoClient } = require('mongodb');
-require('dotenv').config()
-
-
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 const uri = `mongodb+srv://${process.env.DbUser}:${process.env.RRUNmongodbPass}@cluster0.dibao.mongodb.net/${process.env.SixtyNinethStreet}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+client.connect((err) => {
+  const collection = client.db("SixtyNinethStreet").collection("Property");
 
+  console.log("Database Connected bro");
+});
 
-client.connect(err => {
-    const collection = client.db("SixtyNinethStreet").collection("Property");
-
-
-
-    console.log('Database Connected bro');
-  });
-
-
-app.get('/',(req, res)=>{
-    res.send('hello world');
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
 app.listen(process.env.PORT || port);
