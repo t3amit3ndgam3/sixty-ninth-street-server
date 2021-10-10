14308d8939b5a67f871254a79f536a7f7a224727
GolamRabby96 <golamrabbynwu@gmail.com>
2021-10-10 08:38:30 +0600

    done two api

--- index.js
+++ index.js
@@ -1,35 +1,108 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const port = 5000;
-const { MongoClient } = require('mongodb');
-require('dotenv').config()
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const fileUpload = require("express-fileupload");
+
+const uri = `mongodb+srv://${process.env.DbUser}:${process.env.DbPass}@cluster0.dibao.mongodb.net/${process.env.DbName}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 
-
-const uri = `mongodb+srv://${process.env.DbUser}:${process.env.RRUNmongodbPass}@cluster0.dibao.mongodb.net/${process.env.SixtyNinethStreet}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
+// update your .env 
+// DbUser = TeamIt
+// DbPass = TeamItPass0102
+// DbName = SixtyNinethStreet
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static("properties"));
+app.use(fileUpload());
 
+client.connect((err) => {
+	const PropertyDB = client.db("SixtyNinethStreet").collection("Property");
 
+	app.post("/addProperties", (req, res) => {
+		let number = Math.random() * 10000000000;
+		let floorNumber = Math.floor(number);
 
-client.connect(err => {
-    const collection = client.db("SixtyNinethStreet").collection("Property");
+		const property_for = req.body.property_for;
+		const property_type = req.body.property_type;
+		const city = req.body.city;
+		const property_name = req.body.property_name;
+		const property_description = req.body.property_description;
+		const facing = req.body.facing;
+		const property_size = req.body.property_size;
+		const price = req.body.price;
+		const price_is = req.body.Price_is;
+		const bedroom = req.body.bedroom;
+		const garages = req.body.garages;
+		const balconies = req.body.balconies;
+		const bathroom = req.body.bathroom;
+		const owner_name = req.body.owner_name;
+		const owner_number = req.body.owner_number;
+		const owner_gmail = req.body.owner_gmail;
+		const image_one = req.files.image_one;
+		const image_two = req.files.image_two;
+		const image_three = req.files.image_three;
 
+		const key = `${owner_gmail}_${floorNumber}`;
+		const imgOne = `${property_name}_${image_one.name}`;
+		const imgTwo = `${property_name}_${image_two.name}`;
+		const imgThree = `${property_name}_${image_three.name}`;
 
+		image_one.mv(`${__dirname}/properties/${property_name}_${image_one.name}`);
+		image_two.mv(`${__dirname}/properties/${property_name}_${image_two.name}`);
+		image_three.mv(
+			`${__dirname}/properties/${property_name}_${image_three.name}`
+		);
 
-    console.log('Database Connected bro');
-  });
+		PropertyDB.insertOne({
+			key,
+			property_for,
+			property_type,
+			city,
+			property_name,
+			property_description,
+			facing,
+			property_size,
+			price,
+			price_is,
+			bedroom,
+			garages,
+			balconies,
+			bathroom,
+			owner_name,
+			owner_gmail,
+			owner_number,
+			imgOne,
+			imgTwo,
+			imgThree,
+		})
+			.then((result) => {
+				res.send(result.insertedCount > 0)
+			})
+			.then((err) => {
+				console.log(err)
+			})
+	})
 
+  app.get('/allProperty',(req, res) => {
+		PropertyDB.find({}).toArray((err, documents) => {
+			res.send(documents);
+		});
+	})
 
-app.get('/',(req, res)=>{
-    res.send('hello world');
+	console.log("Database Connected bro");
+});
+
+app.get("/", (req, res) => {
+	res.send("hello world");
 });
 
 app.listen(process.env.PORT || port);
